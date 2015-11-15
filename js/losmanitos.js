49d523c0061f7fd9f8204010702b901a819e94e1
p1nox <pinox101@gmail.com>
2015-11-15 09:31:04 -0300

    checking for category elements

--- js/losmanitos.js
+++ js/losmanitos.js
@@ -1,5 +1,31 @@
-var menu = document.getElementById("product-categories").getElementsByTagName("li");
+var $ = jQuery;
 
-for (var i=0; i<menu.length; i++) {
-    alert(menu[i].tagName);
-}
+$(function() {
+
+  var $categories = $('.product-categories li a');
+
+  if (!$categories.length) {
+    return ;
+  }
+
+  var currentPathToCategory = window.location.pathname.split('/').slice(3).join('/');
+
+  $categories.each(function(index, el) {
+    var $el = $(el),
+      pathToCategory = $(el).attr('href');
+
+    if (!pathToCategory) {
+      return ;
+    }
+
+    var pathToCategory = pathToCategory.split('/').slice(5).join('/');
+    console.log(pathToCategory, currentPathToCategory);
+    if (currentPathToCategory === pathToCategory) {
+      // $el.hide();
+      // o
+      // $el.css('display', 'none');
+    }
+  });
+
+});
+
